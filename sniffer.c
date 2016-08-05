2a5d980499831c0676d2283baa01253d951300af
Matt Byers <matt_byers@live.com>
2016-08-05 12:13:20 +1200

    Initial commit

---
+++ sniffer.c
@@ -0,0 +1,35 @@
+/*
+ * sniffer.c
+ *
+ * By David C Harrison (david.harrison@ecs.vuw.ac.nz) July 2015
+ *
+ * Use as-is, modification, and/or inclusion in derivative works is permitted only if
+ * the original author is credited.
+ *
+ * To compile: gcc -o sniffer sniffer.c -l pcap
+ *
+ * To run: tcpdump -s0 -w - | ./sniffer -
+ *     Or: ./sniffer <some file captured from tcpdump or wireshark>
+ */
+
+#include <stdio.h>
+#include <pcap.h>
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    printf("Header Length: %d\n", header->len);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Must have an argument, either a file name or '-'\n");
+        return -1;
+    }
+
+    pcap_t *handle = pcap_open_offline(argv[1], NULL);
+    pcap_loop(handle, 1024*1024, got_packet, NULL);
+    pcap_close(handle);
+
+    return 0;
+}
